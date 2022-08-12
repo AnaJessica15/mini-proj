a0e374a93c3af8db4a1fc77cf319baae2a1453bb
Ana Jessica <anajessica15022002.com>
2022-08-12 09:33:16 +0530

    commit

--- app.py
+++ app.py
@@ -68,8 +68,8 @@ def main():
 
         if result == 0:
             st.error(
-                "Hello: " + fn +" "
-                'according to our aalculations, you will not get the loan from bank'
+                "Hello, " + fn +" "
+                'according to our calculations, you will not get the loan from bank'
             )
         else:
             st.success(
